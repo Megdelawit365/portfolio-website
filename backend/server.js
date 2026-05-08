d4545a8c7baf456fb8eb300830bfef72308d8083
Megdelawit365 <megdelawitaynalem365@gmail.com>
2026-05-08 15:01:54 +0300

    add test route

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,9 @@ app.use(cors({
 app.use(express.json())
 
 app.use("/api/contact", contactRoute)
-
+app.get("/test", (req, res) => {
+    res.json({ message: "Backend is working" })
+})
 
 app.listen(process.env.PORT, () => {
     console.log(`Server running on port ${process.env.PORT}`)
